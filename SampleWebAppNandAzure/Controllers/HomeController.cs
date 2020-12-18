9198f6c14756c8f7d850d15dbfbc5fad9026f43a
Nandini-4599 <Nandini.Rajdev@lntinfotech.com>
2020-12-18 10:03:30 +0530

    WebApp added

---
+++ SampleWebAppNandAzure/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleWebAppNandAzure.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Nandini Rajdev's Page!!!!!";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
